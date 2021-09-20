e70025bc4b6ba0d9a38adccf83677cfc95209ace
lavinas-science <lavinas@gmail.com>
2021-09-20 14:52:02 -0300

    first

---
+++ rest_errors/rest_errors.go
@@ -0,0 +1,27 @@
+package rest_errors
+
+import (
+	"net/http"
+)
+
+type RestErr struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+	Error   string `json:"error"`
+}
+
+func NewRestErr(message string, status int, error string) *RestErr {
+	return &RestErr{message, status, error}
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return NewRestErr(message, http.StatusBadRequest, "bad_request")
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return NewRestErr(message, http.StatusNotFound, "not_found")
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return NewRestErr(message, http.StatusInternalServerError, "internal_server_error")
+}
---
+++ rest_errors/rest_errors_test.go
@@ -0,0 +1,52 @@
+package rest_errors
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewRestErr(t *testing.T) {
+	message := "Message"
+	status := http.StatusBadRequest
+	error := "found" 
+	e := NewRestErr(message, status, error)
+	assert.NotNil(t, e)
+	assert.EqualValues(t, e.Message, message)
+	assert.EqualValues(t, e.Error, error)
+	assert.EqualValues(t, e.Status, status)
+}
+
+func TestNewBadRequestError(t *testing.T) {
+	message := "Message"
+	status := http.StatusBadRequest
+	error := "bad_request" 
+	e := NewBadRequestError(message)
+	assert.NotNil(t, e)
+	assert.EqualValues(t, e.Message, message)
+	assert.EqualValues(t, e.Error, error)
+	assert.EqualValues(t, e.Status, status)
+}
+
+func TestNewNotFoundError(t *testing.T) {
+	message := "Message"
+	status := http.StatusNotFound
+	error := "not_found" 
+	e := NewNotFoundError(message)
+	assert.NotNil(t, e)
+	assert.EqualValues(t, e.Message, message)
+	assert.EqualValues(t, e.Error, error)
+	assert.EqualValues(t, e.Status, status)
+}
+
+func TestNewInternalServerError(t *testing.T) {
+	message := "Message"
+	status := http.StatusInternalServerError
+	error := "internal_server_error" 
+	e := NewInternalServerError(message)
+	assert.NotNil(t, e)
+	assert.EqualValues(t, e.Message, message)
+	assert.EqualValues(t, e.Error, error)
+	assert.EqualValues(t, e.Status, status)
+}
