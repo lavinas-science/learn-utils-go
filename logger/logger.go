f2bd593fbe3adf1b6705432e5c4d65595c63c15a
lavinas-science <lavinas@gmail.com>
2021-09-28 18:22:06 -0300

    logger 2

--- logger/logger.go
+++ logger/logger.go
@@ -2,27 +2,53 @@ package logger
 
 import (
 	"fmt"
-	
+	"os"
+	"strings"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
+
+const (
+	envLogLevel = "LOG_LEVEL"
+	envLogOutput = "LOG_OUTPUT"
+)
+
 
 var (
 	log logger
 )
 
-type logger struct {
+type loggerInterface interface {
+	Print(...interface{})
+	Printf(string, ...interface{})
+}
+
+type logger struct {	
 	log *zap.Logger
 }
 
-func GetLogger() *logger {
-	return &log
+func GetLogger() loggerInterface {
+	return log
 }
+
+func (l logger) Printf(format string, v...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+		return
+	}
+	Info(fmt.Sprintf(format, v...))	
+}
+
+func (l logger) Print(v...interface{}) {
+	Info(fmt.Sprintf("%v", v...))	
+}
+
 
 func init() {
 	logConfig := zap.Config{
-		OutputPaths: []string{"stdout"},
-		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		OutputPaths: []string{getOutput()},
+		Level:       zap.NewAtomicLevelAt(getLevel()),
 		Encoding:    "json",
 		EncoderConfig: zapcore.EncoderConfig{
 			LevelKey:     "level",
@@ -40,6 +66,23 @@ func init() {
 	}
 }
 
+func getOutput() string{
+	ret := os.Getenv(strings.ToLower(strings.TrimSpace(envLogOutput)))
+	if ret == "" {
+		return "output"
+	}
+	return ret
+}
+
+func getLevel() zapcore.Level {
+	switch os.Getenv(strings.ToLower(strings.TrimSpace(envLogLevel))) {
+	case "debug": return zap.DebugLevel
+	case "info": return zap.InfoLevel
+	case "error": return zap.ErrorLevel
+	default: return zap.InfoLevel
+	}
+}
+
 func Info(msg string, tags ...zap.Field) {
 	log.log.Info(msg, tags...)
 	log.log.Sync()
@@ -49,13 +92,4 @@ func Error(msg string, err error, tags ...zap.Field) {
 	tags = append(tags, zap.NamedError("error", err))
 	log.log.Error(msg, tags...)
 	log.log.Sync()
-}
-
-func (l *logger) Printf(format string, v...interface{}) {
-	if len(v) == 0 {
-		Info(format)
-		return
-	}
-	Info(fmt.Sprintf(format, v...))
-	
 }
